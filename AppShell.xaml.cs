307e34c187cc7b429c896b007b11a7102382da7a
Mushtaq M A <mushtaqat3gb@gmail.com>
2022-09-17 23:19:25 +0530

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiRegistration;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiRegistration;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnRegisterClicked(object sender, EventArgs e)
+    {
+        count = count == 1 ? 0 : 1;
+        btn_register.Text = count == 1 ? $"Registered" : "Register";
+        SemanticScreenReader.Announce(btn_register.Text);
+	}
+}
+
